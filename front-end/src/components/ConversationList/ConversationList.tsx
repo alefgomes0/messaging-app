461119d1229af290c6a3c23b39149b3547bd5f7d
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-09-19 21:54:12 -0300

    Make small adjustments

--- front-end/src/components/ConversationList/ConversationList.tsx
+++ front-end/src/components/ConversationList/ConversationList.tsx
@@ -67,7 +67,7 @@ export const ConversationList = () => {
               </div>
             ) : (
               <>
-                <h2 className="text-neutral-100 text-xl font-semibold px-3">
+                <h2 className="text-neutral-100 text-xl font-bold px-3">
                   Conversations
                 </h2>
                 <SearchBar
@@ -78,12 +78,14 @@ export const ConversationList = () => {
                   {conversationListInfo?.map((conversation) => {
                     return (
                       <div
-                        className="min-h-[90px] w-full grid grid-cols-[min_150px] grid-rows-2 items-center gap-x-4 gap-y-1 text-neutral-200 rounded-md hover:bg-neutral-600 px-3"
+                        className="min-h-[90px] w-full grid grid-cols-[60px_1fr] grid-rows-2 items-center gap-x-4 gap-y-1 text-neutral-200 rounded-md hover:bg-neutral-600 transition-colors px-3"
                         key={conversation.participants[0]._id}
                       >
                         <div className="w-[58px] h-[58px] rounded-full bg-blue-600 col-start-1 col-end-2 row-span-full self-center"></div>
-                        <div className="self-end flex justify-between col-start-2 col-end-3 row-start-1 row-end-2">
-                          <h4>{conversation.participants[0].name}</h4>
+                        <div className="w-full self-end flex justify-between col-start-2 col-end-3 row-start-1 row-end-2">
+                          <h4 className="font-bold text-neutral-100">
+                            {conversation.participants[0].name}
+                          </h4>
                           <h6 className="text-xs opacity-60">16:20</h6>
                         </div>
                         <p className="truncate text-sm self-start col-start-2 col-end-3 row-start-2 row-end-3 opacity-70">
