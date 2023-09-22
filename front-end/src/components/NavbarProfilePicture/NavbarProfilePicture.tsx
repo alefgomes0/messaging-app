08d3708b3e5c16054cf662cb99d7b00fb62b125b
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-09-22 13:51:47 -0300

    Add more content to the NavbarProfilePicture

--- front-end/src/components/NavbarProfilePicture/NavbarProfilePicture.tsx
+++ front-end/src/components/NavbarProfilePicture/NavbarProfilePicture.tsx
@@ -8,7 +8,7 @@ export const NavbarProfilePicture = () => {
 
   const listRef = useRef<HTMLLIElement | null>(null);
   const divRef = useRef<HTMLDivElement | null>(null);
-  const inputRef = useRef<HTMLInputElement | null>(null)
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   useEffect(() => {
     const displayMenu = (e: MouseEvent) => {
@@ -30,34 +30,48 @@ export const NavbarProfilePicture = () => {
     };
   }, [showMenu, editName]);
 
+  useEffect(() => {
+    if (editName) inputRef.current?.focus();
+  }, [editName]);
+
   return (
     <li
       ref={listRef}
       className={`w-[40px] h-[40px] rounded-full rounded-lg  flex items-center justify-center cursor-pointer hover:bg-neutral-700 } transition-colors duration-100 ease-in-out`}
+      onClick={() => setShowMenu(true)}
     >
       {showMenu ? (
-        <div className="fixed left-1 bottom-1 z-10 grid grid-cols-1 auto-rows-min gap-y-6 w-96 h-[450px] bg-neutral-700 px-3 pt-12 rounded-md cursor-auto">
+        <div className="fixed left-0 bottom-0 z-10 grid grid-cols-1 auto-rows-min gap-y-6 w-96 h-[450px] bg-neutral-700 px-3 pt-12 rounded-md cursor-auto">
           <div className="w-[75px] h-[75px] rounded-full bg-sky-900"></div>
-          <div className="flex items-center justify-between text-neutral-200" ref={divRef}>
+          <div
+            className="flex items-center justify-between text-neutral-200"
+            ref={divRef}
+          >
             {editName ? (
-              <input
-                type="text"
-                name="name"
-                value={inputValue}
-                className="border-none outline-none bg-neutral-800 w-64 h-8 pl-1 rounded"
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setInputValue(e.target.value)
-                }
-              />
+              <div className="grid grid-cols-1 auto-rows-min gap-y-1">
+                <input
+                  type="text"
+                  name="name"
+                  value={inputValue}
+                  className="border-none outline-none bg-neutral-800 w-64 h-8 pl-1 rounded"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setInputValue(e.target.value)
+                  }
+                  ref={inputRef}
+                />
+                <div className="w-min h-min px-[20px] py-.5 bg-fuchsia-700 text-fuchsia-50 text-sm justify-self-end rounded-lg">{inputValue.length}/25</div>
+              </div>
             ) : (
-              <h3 className="text-xl font-semibold">Name</h3>
+              <h3 className="text-xl font-semibold">{inputValue}</h3>
             )}
-            <div className="hover:bg-neutral-800 p-1 rounded" onClick={() => {
-              setEditName(!editName)
-              inputRef.current?.focus()
-            }}>
-              <EditIcon width={16} height={16} />
-            </div>
+            <button
+              className="hover:bg-neutral-800 p-1 rounded"
+              onClick={() => {
+                setEditName(!editName);
+              }}
+            >
+              <EditIcon width={20} height={20} />
+            </button>
           </div>
           <div className="flex flex-col justify-between text-neutral-200">
             <p className="text-sm opacity-70">User email</p>
@@ -68,10 +82,7 @@ export const NavbarProfilePicture = () => {
           </button>
         </div>
       ) : (
-        <div
-          className="w-[25px] h-[25px] rounded-full bg-sky-900"
-          onClick={() => setShowMenu(true)}
-        ></div>
+        <div className="w-[25px] h-[25px] rounded-full bg-sky-900"></div>
       )}
     </li>
   );
