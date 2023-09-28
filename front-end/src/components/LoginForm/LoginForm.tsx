3c91691f5900585615f3ebf97302165d47cb7514
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-09-28 17:24:30 -0300

    Change to yup from zod, add more content

--- front-end/src/components/LoginForm/LoginForm.tsx
+++ front-end/src/components/LoginForm/LoginForm.tsx
@@ -1,21 +1,35 @@
 import axios from "axios";
 import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
+import * as Yup from "yup";
 
 type Values = {
   email: string;
   password: string;
 };
 
+const LoginSchema = Yup.object().shape({
+  email: Yup.string()
+    .min(4, "Email must have at least 4 characters")
+    .max(50, "Email must have at most 50 characters")
+    .email("Invalid email")
+    .required("Required"),
+  password: Yup.string()
+    .min(5, "Password must have at least 5 characters")
+    .max(30, "Password must have at most 30 characters")
+    .required(),
+});
+
 export const LoginForm = () => {
   const handleOnSubmit = async (formValues: Values) => {
-    try {
+    console.log(formValues)
+/*     try {
       await axios.post(
         "http://locahost:3000/login",
         (formValues.email, formValues.password)
       );
     } catch (err) {
       console.log(err);
-    }
+    } */
   };
 
   return (
@@ -27,6 +41,7 @@ export const LoginForm = () => {
       onSubmit={(values: Values, { setSubmitting }: FormikHelpers<Values>) => {
         handleOnSubmit(values);
       }}
+      validationSchema={LoginSchema}
     >
       {({ isSubmitting }) => (
         <Form className="flex flex-col w-72">
@@ -37,9 +52,9 @@ export const LoginForm = () => {
             type="email"
             name="email"
             placeholder="johndoe@email.com"
-            className="mb-3 text-sm placeholder:text-sm pl-1 h-[26px]"
+            className="mb-3 text-sm placeholder:text-sm pl-1 h-[26px] rounded-sm border-none outline-none focus:ring-2 focus:ring-fuchsia-400"
           />
-          <ErrorMessage name="email" component="div" />
+          <ErrorMessage name="email" component="div" className="text-red-500 text-sm mt-[-9px]"/>
 
           <label htmlFor="password" className="mb-1">
             Password
@@ -47,14 +62,14 @@ export const LoginForm = () => {
           <Field
             type="password"
             name="password"
-            className="mb-3 text-sm placeholder:text-sm pl-1 h-[26px]"
+            className="mb-3 text-sm placeholder:text-sm pl-1 h-[26px] rounded-sm border-none outline-none focus:ring-2 focus:ring-fuchsia-400"
           />
-          <ErrorMessage name="password" component="div" />
+          <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-[-9px]"/>
 
           <button
             type="submit"
             disabled={isSubmitting}
-            className="w-72 h-min py-1 mt-4 rounded-sm bg-fuchsia-800 text-fuchsia-50"
+            className="w-72 h-min py-1 mt-4 rounded-sm bg-fuchsia-800 hover:bg-fuchsia-700 transition-colors text-fuchsia-50 border-none"
           >
             Login
           </button>
