7ec0f520b527af87e2982e4c0f5e52e3c84f05d3
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-10-19 11:34:33 -0300

    Change allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://messaging-app-git-main-alefgomes0.vercel.app/",
-  "https://messaging-jzn3qrum8-alefgomes0.vercel.app/",
+  "https://messaging-app-peach-psi.vercel.app",
+  "https://messaging-app-git-main-alefgomes0.vercel.app",
+  "https://messaging-mvpuxbcxy-alefgomes0.vercel.app",
   "http://127.0.0.1:3000",
 ];
 
