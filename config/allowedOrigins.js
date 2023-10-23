1163e51ae369a714cad35ea692e62ce61164cbbf
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-10-23 13:12:00 -0300

    ..

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,10 @@
 const allowedOrigins = [
+  "http://localhost:5173",
+  "https://messaging-e0tpbkzva-alefgomes0.vercel.app",
   "https://messaging-app-peach-psi.vercel.app",
   "https://messaging-app-git-main-alefgomes0.vercel.app",
-  "https://messaging-mvpuxbcxy-alefgomes0.vercel.app",
+  "https:/messaging-e0tpbkzva-alefgomes0.vercel.app",
+  "http://127.0.0.1:3000",
 ];
 
 module.exports = allowedOrigins;
