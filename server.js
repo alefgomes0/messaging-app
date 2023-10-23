62621c7a373337b784f1d22519d1fb0594332841
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-10-23 13:43:21 -0300

    !

--- server.js
+++ server.js
@@ -45,19 +45,10 @@ const server = app.listen(PORT, () =>
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: [
-      "https://messaging-app-peach-psi.vercel.app",
-      "https://messaging-app-git-main-alefgomes0.vercel.app",
-      "https:/messaging-e0tpbkzva-alefgomes0.vercel.app",
-      "http://localhost:5173",
-      "https://messaging-e0tpbkzva-alefgomes0.vercel.app",
-      "https://messaging-app-peach-psi.vercel.app",
-      "https://messaging-app-git-main-alefgomes0.vercel.app",
-      "https:/messaging-e0tpbkzva-alefgomes0.vercel.app",
-      "http://127.0.0.1:3000",
-    ],
+    origin: "https://messaging-app-5mr7.onrender.com",
     withCredentials: true,
   },
+  withCredentials: true
 });
 
 let connectedUsers = [];
