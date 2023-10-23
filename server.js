7fcc2df8cfaa768375e8f22c32fc89be02d1dab5
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-10-23 12:48:33 -0300

    .

--- server.js
+++ server.js
@@ -46,11 +46,9 @@ const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
     origin: [
-      "http://localhost:5173",
       "https://messaging-app-peach-psi.vercel.app",
       "https://messaging-app-git-main-alefgomes0.vercel.app",
       "https://messaging-mvpuxbcxy-alefgomes0.vercel.app",
-      "http://127.0.0.1:3000",
     ],
     withCredentials: true,
   },
