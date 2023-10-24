c347a58888a5246dc8259e343ee52fa8f9aaa982
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-10-24 16:48:30 -0300

    .

--- front-end/src/api/axios.ts
+++ front-end/src/api/axios.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
-const BASE_URL = "https://messaging-app-5mr7.onrender.com";
+const BASE_URL =  "https://messaging-app-5mr7.onrender.com"
 
 export default axios.create({
-  baseURL: BASE_URL,
+  baseURL: BASE_URL
 });
 
-export const axiosPrivate = axios.create({
+export const axiosPrivate =  axios.create({
   baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true,
+  withCredentials: true
 });
