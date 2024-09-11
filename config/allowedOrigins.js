07cabf2f9f6af06f62e850edf823eee9167791d1
alexandre <suporteprimebusiness@gmail.com>
2024-09-11 01:43:54 -0300

    muda origens

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,17 @@
-const allowedOrigins = process.env.ORIGENS.split(", ");
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://127.0.0.1:3000",
+  "https://messaging-e0tpbkzva-alefgomes0.vercel.app",
+  "https://messaging-app-peach-psi.vercel.app",
+  "https://messaging-app-git-main-alefgomes0.vercel.app",
+  "https://sp-fe-jet.vercel.app",
+  "https://sp-fe-git-main-alefgomes0s-projects.vercel.app",
+  "https://sp-co9uz7dt9-alefgomes0s-projects.vercel.app",
+  "https://sp-duyhwr2pz-alefgomes0s-projects.vercel.app",
+  "priscipsi.com.br",
+  "www.priscipsi.com.br",
+  "https://www.priscipsi.com.br",
+  "sp-ad.vercel.app"
+];
 
 module.exports = allowedOrigins;
