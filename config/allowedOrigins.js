515103b4929f7119d8a548d8a94e1d214d856be5
alexandre <suporteprimebusiness@gmail.com>
2024-09-11 00:49:29 -0300

    w

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,16 +1,3 @@
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://127.0.0.1:3000",
-  "https://messaging-e0tpbkzva-alefgomes0.vercel.app",
-  "https://messaging-app-peach-psi.vercel.app",
-  "https://messaging-app-git-main-alefgomes0.vercel.app",
-  "https://sp-fe-jet.vercel.app",
-  "https://sp-fe-git-main-alefgomes0s-projects.vercel.app",
-  "https://sp-co9uz7dt9-alefgomes0s-projects.vercel.app",
-  "https://sp-duyhwr2pz-alefgomes0s-projects.vercel.app",
-  "priscipsi.com.br",
-  "www.priscipsi.com.br",
-  "https://www.priscipsi.com.br"
-];
+const allowedOrigins = process.env.ORIGENS.trim().split(",");
 
 module.exports = allowedOrigins;
