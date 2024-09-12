a9f6e273927d0d4dbe96b19a1828cbbe079e81d8
alexandre <suporteprimebusiness@gmail.com>
2024-09-12 20:18:06 -0300

    dswqe

--- controllers/testemunhoController.js
+++ controllers/testemunhoController.js
@@ -15,4 +15,28 @@ exports.get = async function (req, res, next) {
   }
 };
 
+exports.post = [
+  body("mensagem").escape().trim(),
+  async (req, res, next) => {
+    const novaMensagem = new Mensagem({
+      iniciais_nome: req.body.nome,
+      mensagem: req.body.mensagem,
+      data: req.body.data,
+      visivel: false,
+    });
 
+    try {
+      await novaMensagem.save();
+    } catch (err) {
+      return res.status(500).json({
+        sucesso: false,
+        mensagem: "Não foi possível enviar sua mensagem.",
+        erro: err.message,
+      });
+    }
+    return res.status(200).json({
+      sucesso: true,
+      mensagem: "Mensagem enviada com sucesso!",
+    });
+  },
+];
--- routes/testemunho.js
+++ routes/testemunho.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const  testemunho  = require("../controllers/testemunhoController");
 
 router.get("/", testemunho.get);
+router.post("/", testemunho.post);
 
 module.exports = router;
